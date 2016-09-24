47ac3dd1df83f7fe753d03c0d1325cebbcdc68ff
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2016-09-24 23:58:27 +0200

    Working to replace the aar shell script

--- README.md
+++ README.md
@@ -7,11 +7,24 @@ A Leiningen plugin to ease the development of Android .aar projects
 Put `[android_development "0.1.0-SNAPSHOT"]` into the `:plugins`
 vector of your project.clj.
 
-Create a aar from the code:
+You will need to add some configuration values to the `project.clj`:
+
+    :android-jar  specify the location of the "android.jar" file
+    :aapt         path to the "aapt" command to use
+	:aar-name     path to the destination .aar file
+
+**TODO/FIXME** include the mechanism to find out the locations
+automatically if not specified
+
+also you need to specify
+
+    :aot :all
+
+in the options, and then you can create a aar from the code:
 
     $ lein android_development aar
 
-(it will trigger the compilation of a jar).
+which will trigger the compilation of the jar, first.
 
 Watch for changes in the resources and update the `R.java` file as a
 consequence:
--- src/leiningen/android_development.clj
+++ src/leiningen/android_development.clj
@@ -1,8 +1,54 @@
 (ns leiningen.android_development
-  (use [leiningen.core.main :only [info]]))
+  (use [leiningen.core.main :only [info abort]]
+       [clojure.java.shell :only [sh]]))
+
+
+(defn- get-arguments [project xs]
+  "Read the arguments from the project, fail if any is missing"
+  (info "Ensuring that the project arguments are there")
+  (reduce (fn [m v]
+            (if (contains? project v)
+              (assoc m v (get project v))
+              (abort (str "Unable to find " v " in project.clj!"))))
+          {}
+          xs
+          ))
+
+(defn- check-or-create-directory! [path]
+  (info "Checking for " path "existance")
+  )
+
 
 (defn- create-aar [project arguments]
-  (info "create-aar executed"))
+  (let [my-args (get-arguments project [:android-jar :aapt :aar-name :aot])]
+    (if (not= (:aot my-args) [:all])
+      (abort (str ":aot :all must be specified in project.clj!" (:android-jar my-args)))
+      (do
+        ;;; TODO/FIXME: get the plugin in a sander way here (use the jar function directly?)
+        (let [jar-path (second (first (leiningen.core.main/apply-task "jar" project [])))]
+          (info "The jar is: " jar-path)
+          (check-or-create-directory! "src")
+          (check-or-create-directory! "res")
+          (info "create-aar executed")
+          ;;; RUN AAPT
+;;;; # The --output-text-symbols is paramento
+;;;; ${AAPT} package \
+;;;; 	--output-text-symbols ${DEV_HOME} \
+;;;; 	-v \
+;;;; 	-f \
+;;;; 	-m \
+;;;; 	-M ${DEV_HOME}/AndroidManifest.xml \
+;;;; 	-I ${ANDROID_JAR} \
+;;;; 	-S ${DEV_HOME}/res \
+;;;; 	-J ${DEV_HOME}/src \
+;;;; 
+;;;; # zip the aar file
+;;;; echo "zipping the aar content into ${AAR_TEMP_FILE}…"
+;;;; rm ${AAR_TEMP_FILE}
+;;;; zip -r ${AAR_TEMP_FILE} classes.jar R.txt res AndroidManifest.xml
+;;;; mv ${AAR_TEMP_FILE} ${AAR_DESTINATION}
+;;;;           
+          )))))
 
 (defn android_development
   "Create a aar file from a jar"
@@ -10,4 +56,4 @@
   (case (first args) 
     nil (info "An action must be provided")
     "aar" (create-aar project (rest args))
-    (info "Unknown option")))
+    (abort "Unknown option")))
