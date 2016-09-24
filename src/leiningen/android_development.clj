d5a150c18652c64c15a282378a37b31ab74d0ec8
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2016-09-24 19:07:14 +0200

    Added subtasks to the plugin
    Just spiking, so everything will have to be rewritten :p

--- src/leiningen/android_development.clj
+++ src/leiningen/android_development.clj
@@ -1,6 +1,13 @@
-(ns leiningen.android_development)
+(ns leiningen.android_development
+  (use [leiningen.core.main :only [info]]))
+
+(defn- create-aar [project arguments]
+  (info "create-aar executed"))
 
 (defn android_development
-  "I don't do a lot."
+  "Create a aar file from a jar"
   [project & args]
-  (println "Hi!"))
+  (case (first args) 
+    nil (info "An action must be provided")
+    "aar" (create-aar project (rest args))
+    (info "Unknown option")))
